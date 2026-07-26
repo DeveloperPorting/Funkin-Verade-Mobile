cca44f0c0d5ad5591855abe8928c77da2b687e9f
StarNovaBR <cherrylegal001@gmail.com>
2026-07-26 08:28:46 -0300

    mds mano

--- setup/unix.sh
+++ setup/unix.sh
@@ -15,7 +15,7 @@ mkdir ~/haxelib && haxelib setup ~/haxelib
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
 haxelib git lime https://github.com/DeveloperPorting-Stuff/lime main
-hadelib git hxcpp https://github.com/MaysLastPlays-Stuff/hxcpp-h master
+haxelib git hxcpp https://github.com/MaysLastPlays-Stuff/hxcpp-h master
 haxelib install openfl 9.4.1
 haxelib install flixel 6.1.0 --skip-dependencies
 haxelib install flixel-addons 3.3.2 --skip-dependencies
